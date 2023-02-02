1fa236ea10d19ea74fe8047db5fade9788fe03e1
Tom Radford <tom@theradford.com>
2023-02-02 13:40:12 +0200

    feat: lazy loading

--- src/lib/services/shows.ts
+++ src/lib/services/shows.ts
@@ -6,9 +6,8 @@ const getUrl = (entity: string) => {
 		return `https://api.themoviedb.org/3/${entity}?api_key=${clientEnv.NEXT_PUBLIC_TMDB_KEY}`
 	} else throw new Error('API key missing')
 }
-export const fetchPopularShows = async ({ queryKey }) => {
-	console.log(queryKey)
-	const res = await axios.get(getUrl('/tv/popular'))
+export const fetchPopularShows = async ({ pageParam = 1 }) => {
+	const res = await axios.get(`${getUrl(`/tv/popular`)}&page=${pageParam}`)
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 	return res.data
 }
--- src/pages/shows.tsx
+++ src/pages/shows.tsx
@@ -1,24 +1,43 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { fetchPopularShows } from '../lib/services/shows'
 import Show from '../components/Show'
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
 
 const ShowsPage = () => {
-	const { isLoading, isError, data, error } = useQuery(
-		['popularShows'],
-		fetchPopularShows
-	)
+	// const [page, setPage] = useState(1)
+	const { inView, ref } = useInView()
+	const {
+		data,
+		error,
+		fetchNextPage,
+		hasNextPage,
+		isFetching,
+		isFetchingNextPage,
+		status,
+	} = useInfiniteQuery({
+		queryKey: ['popularShows'],
+		queryFn: fetchPopularShows,
+		getNextPageParam: (lastPage, pages) => lastPage.page + 1,
+	})
 
-	if (isLoading) {
-		return <p>loading</p>
+	if (isFetching) {
+		return (
+			<Layout>
+				<p>loading</p>
+			</Layout>
+		)
 	}
 
-	if (isError) {
-		return <p>Error</p>
-	}
+	// if (isError) {
+	// 	return <p>Error</p>
+	// }
 
-	console.log(data)
+	if (inView) {
+		fetchNextPage()
+	}
 
 	return (
 		<Layout>
@@ -30,9 +49,18 @@ const ShowsPage = () => {
 					Popular shows!
 				</h1>
 				<div className="mt-10 grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
-					{data.results.map((show) => (
-						<Show show={show} key={show.id} />
+					{data?.pages.map((group, i) => (
+						<>
+							<React.Fragment key={i}>
+								{group.results.map((show) => (
+									<Show show={show} key={show.id} />
+								))}
+							</React.Fragment>
+						</>
 					))}
+
+					<div ref={ref}></div>
+					{/* <button onClick={() => fetchNextPage()}>more!</button> */}
 				</div>
 			</main>
 		</Layout>
